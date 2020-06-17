ba966ec47c5ebd104d69e9ac2c833e9d38e11b37
ParthS <41526125+ParthS94@users.noreply.github.com>
2020-06-16 23:10:15 -0500

    Predicted State and Initial Process Covariance Matrix

---
+++ KalmanFilter.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+positionObserved = np.array([4000, 4260, 4550, 4860, 5110])
+velocityObserved = np.array([280, 282, 285, 286, 290])
+
+a = np.array([2])
+x_init = positionObserved[0]
+v_init = velocityObserved[0]
+t = 1
+
+stateMatrix = np.array([[1, t], [0, 1]])
+controlMatrix = np.array([[1 / 2 * (t ** 2)], [t]])
+
+# Process errors from the process covaraince matrix
+x_cvError = 20
+v_cvError = 5
+
+# Observation errors
+x_obError = 25
+v_obError = 6
+
+
+def GeneratePredictedState(i):
+    stateVector = np.array([[positionObserved[i]], [velocityObserved[i]]])
+    vec1 = np.matmul(stateMatrix, stateVector)
+    vec2 = a * controlMatrix
+    newState = np.add(vec1, vec2)
+    return newState
+
+
+predicted_state = GeneratePredictedState(0)
+
+
+def GenerateProcessCovariance(positionError, velocityError):
+    return np.array([[positionError ** 2, 0], [0, velocityError ** 2]])
+
+processCovMatrix = GenerateProcessCovariance(x_cvError, v_cvError)
+
+
